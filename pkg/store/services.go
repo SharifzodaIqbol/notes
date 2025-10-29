8bc694ddf797027cda74c23e7db039f1b762b26d
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2025-10-29 23:19:19 +0300

    contain basic operations

---
+++ pkg/store/services.go
@@ -0,0 +1,65 @@
+// Package store содержит общие утилиты для работы с БД и HTTP.
+package store
+
+import (
+	"database/sql"
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+type Notes struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+type ViewData struct {
+	Title   string
+	Content string
+}
+type PageData struct {
+	PageTitle string
+	Notes     []ViewData
+}
+
+func GetAllNotes(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		json.NewEncoder(w).Encode(map[string]string{"error": "Method not allowed"})
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM notes")
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]string{"error": "Database query error"})
+		return
+	}
+	defer rows.Close()
+	notes := []ViewData{}
+	for rows.Next() {
+		var note Notes
+		err := rows.Scan(&note.ID, &note.Title, &note.Content, &note.CreatedAt, &note.UpdatedAt)
+		if err != nil {
+			json.NewEncoder(w).Encode(map[string]string{"error": "Error iterating rows" + err.Error()})
+			return
+		}
+		data := ViewData{
+			Title:   note.Title,
+			Content: note.Content,
+		}
+		notes = append(notes, data)
+	}
+	pageData := PageData{
+		PageTitle: "Мои заметки",
+		Notes:     notes,
+	}
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Error loading template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, pageData)
+	w.WriteHeader(http.StatusOK)
+}
